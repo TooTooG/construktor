6d7386938985613d90ec7022d1cca7211e8c4663
TooTooG <soimavladik@gmail.com>
2026-05-18 14:39:57 +0300

    Add backend server entrypoint

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import { buildApp } from "./app.js";
+import { env } from "./config/env.js";
+
+const app = buildApp();
+
+app
+  .listen({ port: env.PORT, host: "0.0.0.0" })
+  .then(() => {
+    app.log.info({ port: env.PORT }, "server_started");
+  })
+  .catch((error) => {
+    app.log.error(error, "server_start_failed");
+    process.exit(1);
+  });
